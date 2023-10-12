b592fdb879caa4d2719287370ffd1d5b16c6805d
Ivanova Daria <wwwvovan05@gmail.com>
2023-10-12 14:55:17 +0500

    5 лаба

--- IvanovaAvtoservice — копия/IvanovaAvtoservice/ServicePage.xaml.cs
+++ IvanovaAvtoservice — копия/IvanovaAvtoservice/ServicePage.xaml.cs
@@ -25,11 +25,71 @@ namespace IvanovaAvtoservice
             InitializeComponent();
             var currentServisee = Ivanova_carserviceEntities1.GetContext().Servisee.ToList();
             ServiceListView.ItemsSource = currentServisee;
+            ComboType.SelectedIndex = 0;
+            UpdateServices();
+           
+
+        }
+    private void UpdateServices()
+    {
+        var currentServisee = Ivanova_carserviceEntities1.GetContext().Servisee.ToList();
+        if (ComboType.SelectedIndex == 0)
+        { currentServisee = currentServisee.Where(p => (p.Discount >= 0 && p.Discount <= 100)).ToList(); }
+        if (ComboType.SelectedIndex == 1)
+        { currentServisee = currentServisee.Where(p => (p.Discount >= 0 && p.Discount < 5)).ToList(); }
+        if (ComboType.SelectedIndex == 2)
+        {
+            currentServisee = currentServisee.Where(p => (p.Discount >= 5 && p.Discount < 15)).ToList();
+
+        }
+        if (ComboType.SelectedIndex == 3)
+        {
+            currentServisee = currentServisee.Where(p => (p.Discount >= 15 && p.Discount < 30)).ToList();
+        }
+        if (ComboType.SelectedIndex == 4)
+        {
+            currentServisee = currentServisee.Where(p => (p.Discount >= 30 && p.Discount < 70)).ToList();
+        }
+        if (ComboType.SelectedIndex == 5)
+        {
+            currentServisee = currentServisee.Where(p => (p.Discount >= 70 && p.Discount < 100)).ToList();
+        }
+        currentServisee = currentServisee.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+        if (RButtonDown.IsChecked.Value)
+        {
+            currentServisee = currentServisee.OrderByDescending(p => p.Cost).ToList();
+        }
+        if (RButtonUp.IsChecked.Value)
+        {
+            currentServisee = currentServisee.OrderBy(p => p.Cost).ToList();
+        }
+            ServiceListView.ItemsSource = currentServisee;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+    private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+       
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
         }
     }
 }
